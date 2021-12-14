6fe6ed1b6a61d327321747472eff904ef38ec1c8
Ennaox <alexis.laplanche@hotmail.fr>
2021-12-14 16:29:00 +0100

    Ajout cblas_dgbmv

--- TP5-code/src/tp2_poisson1D_direct.c
+++ TP5-code/src/tp2_poisson1D_direct.c
@@ -8,7 +8,17 @@
 void print_GB(int size, double * AB)
 {
   for(int i = 0; i<size; i++)
-    printf("%lf %d\n",AB[i],i);
+    printf("%.16lf %d\n",AB[i],i);
+}
+
+int comp_vect(double *A, double *B,int size)
+{
+  for(int i =0; i<size;i++)
+  {
+    if(A[i]!=B[i])
+      return 0;
+  }
+  return 1;
 }
 
 int main(int argc,char *argv[])
@@ -23,7 +33,7 @@ int main(int argc,char *argv[])
   int info;
   int NRHS;
   double T0, T1;
-  double *RHS, *EX_SOL, *X;
+  double *RHS, *EX_SOL, *X, *RHS_cpy;
   double *AB;
 
   double temp, relres;
@@ -38,9 +48,11 @@ int main(int argc,char *argv[])
   RHS=(double *) malloc(sizeof(double)*la);
   EX_SOL=(double *) malloc(sizeof(double)*la);
   X=(double *) malloc(sizeof(double)*la);
+  RHS_cpy = malloc(la * sizeof(double));
 
   set_grid_points_1D(X, &la);
   set_dense_RHS_DBC_1D(RHS,&la,&T0,&T1);
+  set_dense_RHS_DBC_1D(RHS_cpy,&la,&T0,&T1);
   set_analytical_solution_DBC_1D(EX_SOL, X, &la, &T0, &T1);
   
   write_vec(RHS, &la, "RHS.dat");
@@ -59,19 +71,61 @@ int main(int argc,char *argv[])
   /* working array for pivot used by LU Factorization */
   ipiv = (int *) calloc(la, sizeof(int));
 
-  int row = 1; //
+  int row = 0; //
+  double alpha = 1;
+  double beta = -1;
+  int incx = 1;
+  int incy = 1;
+  double * Y = malloc(la*sizeof(double));
 
   if (row == 1){ // LAPACK_ROW_MAJOR
+    //Calcul de la solution de A * x = B pour une matrice poisson 1D en format GB Row Major
     set_GB_operator_rowMajor_poisson1D(AB, &lab, &la,&kv);
     write_GB_operator_rowMajor_poisson1D(AB, &lab, &la, "AB_row.dat");
     print_GB(lab*la,AB);
+    
     info = LAPACKE_dgbsv(LAPACK_ROW_MAJOR,la, kl, ku, NRHS, AB, la, ipiv, RHS, NRHS);
+    
+    //Calcul de y = alpha * A + beta * y pour une matrice poisson 1D en format GB Row Major
+    free(AB);
+    kv = 0;
+    lab=kv+kl+ku+1;
+
+    AB = (double *) malloc(sizeof(double)*lab*la);
+    set_GB_operator_rowMajor_poisson1D(AB, &lab, &la,&kv);
+
+    cblas_dgbmv(CblasRowMajor,CblasNoTrans,la,la,kl,ku,alpha,AB,lab,EX_SOL,incx,beta,RHS_cpy,incy);
+    printf("\n");
+    print_GB(la,RHS_cpy);
+
   }
   else { // LAPACK_COL_MAJOR
+    //Calcul de la solution de A * x = B pour une matrice poisson 1D en format GB Col Major
     set_GB_operator_colMajor_poisson1D(AB, &lab, &la, &kv);
     write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "AB_col.dat");
     print_GB(lab*la,AB);
+
     info = LAPACKE_dgbsv(LAPACK_COL_MAJOR,la, kl, ku, NRHS, AB, lab, ipiv, RHS, la);
+
+    //Calcul de y = alpha * A + beta * y pour une matrice poisson 1D en format GB Col Major
+    free(AB);
+    kv = 0;
+    lab=kv+kl+ku+1;
+
+    AB = (double *) malloc(sizeof(double)*lab*la);
+    set_GB_operator_rowMajor_poisson1D(AB, &lab, &la,&kv);
+
+    //Faire appelle a DGBMV
+    free(AB); //Réécriture de AB sans la ligne de 0
+    kv = 0;
+    lab=kv+kl+ku+1;
+    AB = (double *) malloc(sizeof(double)*lab*la);
+
+    set_GB_operator_colMajor_poisson1D(AB, &lab, &la,&kv);
+
+    cblas_dgbmv(CblasColMajor,CblasNoTrans,la,la,kl,ku,alpha,AB,lab,EX_SOL,incx,beta,RHS_cpy,incy);
+    printf("\n");
+    print_GB(la,RHS_cpy);
   }    
 
   
